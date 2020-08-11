f58853e7f65357e748b812701b882cf32f2d4e99
KyWilkinson <wilki018@hotmail.com>
2020-08-11 08:22:39 -0500

    Created methods to call and print to console for the herd and fleet

--- RobotsVsDinosaurs/Battlefield.cs
+++ RobotsVsDinosaurs/Battlefield.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,36 @@ namespace RobotsVsDinosaurs
         //Call all methods here to run game
         public void RunGame()
         {
+            Console.WriteLine("Robots Vs Dinosaurs!");
+            Console.WriteLine("Please press enter to start");
+            Console.ReadLine();
+            
             
         }
+
+        public void CallHerd(List<Dinosaur> herd)
+        {
+            foreach(Dinosaur item in herd)
+            {
+                Console.WriteLine(item.dinosaurName);
+                Console.WriteLine(item.dinosaurHealth);
+                Console.WriteLine(item.dinosaureEnergy);
+                Console.WriteLine(item.dinosaurAttackPower);
+                Console.WriteLine(item.dinosaurAttackType);
+            }
+
+        }
+        public void CallFleet(List<Robot> fleet)
+        {
+            foreach(Robot item in fleet)
+            {
+                Console.WriteLine(item.robotName);
+                Console.WriteLine(item.robotHealth);
+                Console.WriteLine(item.robotPower);
+                Console.WriteLine(item.robotAttackPower);
+                Console.WriteLine(item.robotAttackPower);
+            }
+        }
+
     }
 }
